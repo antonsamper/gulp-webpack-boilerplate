f742a7926062fbbec48e7f011f59b0158cee303f
Anton Samper Rivaya <anton@aeser.co.uk>
2015-08-24 08:46:26 +0100

    fix(helloWorld): improving `helloWorld` component syntax, test and comments fixes #22

--- src/js/components/helloWorld/helloWorld.js
+++ src/js/components/helloWorld/helloWorld.js
@@ -1,10 +1,20 @@
+/*
+ * @title helloWorld
+ * @description Module definition for the helloWorld component
+ */
+
+
+/*********************************************************************************
+ 1. MODULE DEFINITION
+ *********************************************************************************/
+
 (function (app) {
 
   app.helloWorld = function (planet) {
     var hello = 'Hello ';
     var thing = 'World!';
 
-    if(planet) {
+    if(planet === true) {
       thing = 'Planet!';
     }
 
--- src/js/components/helloWorld/helloWorld.spec.js
+++ src/js/components/helloWorld/helloWorld.spec.js
@@ -1,11 +1,21 @@
-//sample test
-describe('helloWorld', function() {
+/*
+ * @title helloWorld
+ * @description Tests for the helloWorld component
+ */
 
-  it('should return Hello World!', function() {
+
+/*********************************************************************************
+ 1. TESTS
+ *********************************************************************************/
+
+
+describe('The helloWorld component...', function() {
+
+  it('should return a `Hello World!` string by default', function() {
     expect(APP.helloWorld()).toEqual('Hello World!');
   });
 
-  it('should return Hello Planet!', function() {
+  it('should return a `Hello Planet!` string when `true` (bool) is passed in', function() {
     expect(APP.helloWorld(true)).toEqual('Hello Planet!');
   });
 
