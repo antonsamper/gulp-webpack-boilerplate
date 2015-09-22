7c5cf511f79d5a826019c835b43f5acdbe636ffd
Anton Samper Rivaya <anton@aeser.co.uk>
2015-09-22 22:01:08 +0100

    chore(gulp): converting to es6

--- gulpfile.babel.js/tasks/eslint.js
+++ gulpfile.babel.js/tasks/eslint.js
@@ -9,23 +9,17 @@
  1. DEPENDENCIES
  *********************************************************************************/
 
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var plumber = require('gulp-plumber');
-var sharedPaths = require('../shared/paths.js');
-var sharedEvents = require('../shared/events.js');
+import eslint from 'gulp-eslint';
 
 
 /*********************************************************************************
  2. TASK
  *********************************************************************************/
 
-gulp.task('eslint', function () {
+gulp.task('eslint', () => {
   return gulp
     .src(sharedPaths.eslintSrc)
-    .pipe(plumber({
-      errorHandler: sharedEvents.onError
-    }))
+    .pipe(plumber({errorHandler: sharedEvents.onError}))
     .pipe(eslint())
     .pipe(eslint.format());
 });
