86a3a1cecb92244f9d93c939fdc12b1f0e3cf942
Anton Samper Rivaya <anton@aeser.co.uk>
2015-09-23 07:08:20 +0100

    chore(gulp): converting to es6

--- gulpfile.babel.js/tasks/iconfont/index.js
+++ gulpfile.babel.js/tasks/iconfont/index.js
@@ -9,14 +9,10 @@
  1. DEPENDENCIES
  *********************************************************************************/
 
-var configIconfont = require('../../config/iconfont.js');
-var generator = require('./generator.js');
-var gulp = require('gulp');
-var iconfont = require('gulp-iconfont');
-var plumber = require('gulp-plumber');
-var sharedPaths = require('../../shared/paths.js');
-var sharedEvents = require('../../shared/events.js');
-var size = require('gulp-size');
+import configIconfont from '../../config/iconfont.js';
+import generator from './generator.js';
+import iconfont from 'gulp-iconfont';
+import size from 'gulp-size';
 
 
 /*********************************************************************************
@@ -27,15 +23,13 @@ gulp.task('iconfont', function () {
   return gulp
     .src(sharedPaths.srcIconFont)
     .pipe(size({showFiles: true}))
-    .pipe(plumber({
-      errorHandler: sharedEvents.onError
-    }))
+    .pipe(plumber({errorHandler: sharedEvents.onError}))
     .pipe(iconfont({
       fontName: configIconfont.name,
       appendCodepoints: configIconfont.codepoints,
       normalize: configIconfont.normalize
     }))
     .on('codepoints', generator)
-    .pipe(gulp.dest(sharedPaths.outputDir + '/fonts'))
+    .pipe(gulp.dest(`${ sharedPaths.outputDir }/fonts`))
     .pipe(size({showFiles: true}));
 });
