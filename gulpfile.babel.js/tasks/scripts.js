da855c0cc78f255d861ce92b5e4f778c9d82f67b
Anton Samper Rivaya <anton@aeser.co.uk>
2016-02-18 07:58:25 +0000

    chore(gulp): explicitly importing required modules

--- gulpfile.babel.js/tasks/scripts.js
+++ gulpfile.babel.js/tasks/scripts.js
@@ -9,15 +9,19 @@
  1. DEPENDENCIES
  *********************************************************************************/
 
-import babel from 'gulp-babel';
-import bowerFiles from 'main-bower-files';
-import concat from 'gulp-concat';
-import cache from 'gulp-cached';
-import gulpif from 'gulp-if';
-import merge from 'merge-stream';
-import rev from 'gulp-rev';
-import uglify from 'gulp-uglify';
-import sourcemaps from 'gulp-sourcemaps';
+import sharedPaths  from '../shared/paths.js';
+import sharedEvents from '../shared/events.js';
+import gulp         from 'gulp';
+import babel        from 'gulp-babel';
+import cache        from 'gulp-cached';
+import concat       from 'gulp-concat';
+import gulpif       from 'gulp-if';
+import plumber      from 'gulp-plumber';
+import rev          from 'gulp-rev';
+import sourcemaps   from 'gulp-sourcemaps';
+import uglify       from 'gulp-uglify';
+import bowerFiles   from 'main-bower-files';
+import merge        from 'merge-stream';
 
 
 /*********************************************************************************
@@ -26,27 +30,27 @@ import sourcemaps from 'gulp-sourcemaps';
 
 export default () => {
 
-  let libs = gulp
-    .src(bowerFiles('**/*.js'))
-    .pipe(plumber({errorHandler: sharedEvents.onError}))
-    .pipe(gulpif(gulpboilerplate.uglify, uglify()))
-    .pipe(gulpif(gulpboilerplate.concat, concat('libs.min.js')))
-    .pipe(gulpif(gulpboilerplate.rev, rev()))
-    .pipe(gulp.dest(`${ sharedPaths.outputDir }/js/bower`));
+    let libs = gulp
+        .src(bowerFiles('**/*.js'))
+        .pipe(plumber({errorHandler: sharedEvents.onError}))
+        .pipe(gulpif(gulpboilerplate.uglify, uglify()))
+        .pipe(gulpif(gulpboilerplate.concat, concat('libs.min.js')))
+        .pipe(gulpif(gulpboilerplate.rev, rev()))
+        .pipe(gulp.dest(`${ sharedPaths.outputDir }/js/bower`));
 
-  let app = gulp
-    .src(sharedPaths.concatSrc)
-    .pipe(plumber({errorHandler: sharedEvents.onError}))
-    .pipe(gulpif(gulpboilerplate.cache, cache('scripts')))
-    .pipe(gulpif(gulpboilerplate.sourcemaps, sourcemaps.init()))
-    .pipe(babel())
-    .pipe(gulpif(gulpboilerplate.sourcemaps, sourcemaps.write()))
-    .pipe(gulpif(gulpboilerplate.uglify, uglify()))
-    .pipe(gulpif(gulpboilerplate.concat, concat('app.min.js')))
-    .pipe(gulpif(gulpboilerplate.rev, rev()))
-    .pipe(gulp.dest(`${ sharedPaths.outputDir }/js`));
+    let app = gulp
+        .src(sharedPaths.concatSrc)
+        .pipe(plumber({errorHandler: sharedEvents.onError}))
+        .pipe(gulpif(gulpboilerplate.cache, cache('scripts')))
+        .pipe(gulpif(gulpboilerplate.sourcemaps, sourcemaps.init()))
+        .pipe(babel())
+        .pipe(gulpif(gulpboilerplate.sourcemaps, sourcemaps.write()))
+        .pipe(gulpif(gulpboilerplate.uglify, uglify()))
+        .pipe(gulpif(gulpboilerplate.concat, concat('app.min.js')))
+        .pipe(gulpif(gulpboilerplate.rev, rev()))
+        .pipe(gulp.dest(`${ sharedPaths.outputDir }/js`));
 
-  return merge(libs, app);
+    return merge(libs, app);
 
 };
 
